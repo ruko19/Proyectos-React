675e8b9408c5494b0ba841fc3b0ed559e9faa3eb
ruko19 <rukobaruna23@gmail.com>
2025-01-11 14:32:42 -0500

    correcion ecomerce react

--- e-comerce_react/src/main.jsx
+++ e-comerce_react/src/main.jsx
@@ -6,7 +6,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+
     <App />
-  </StrictMode>,
+  
 )
